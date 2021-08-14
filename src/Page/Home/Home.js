9d6eb7e2dbebbd6e0f90ec9f9316b94e473484d6
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2021-08-14 10:15:50 +0400

    feat: add new page: <Home/>

---
+++ src/Page/Home/Home.js
@@ -0,0 +1,38 @@
+//CSS
+import "./Home.scss";
+
+// ASSETS
+
+// DEPENDANCIES
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import axios from "axios";
+
+// COMPONENTS
+import Card from "../../Components/Card/Card";
+
+// HOOKS
+import { useState, useEffect } from "react";
+
+const Home = () => {
+  const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(async () => {
+    const url = "http://localhost:5000/";
+    const response = await axios.get(url + "characters");
+
+    setCharacters(response.data.data);
+
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <div className="home">
+      {isLoading
+        ? "Chargement en cours..."
+        : characters.map((e, i) => <Card key={i} title={e.name} />)}
+    </div>
+  );
+};
+
+export default Home;
