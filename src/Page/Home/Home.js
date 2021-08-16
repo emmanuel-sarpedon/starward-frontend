d3e01bc1322a262b0d93dcc88f592c3b49961623
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2021-08-16 12:10:56 +0400

    feat: allow user to manage pagination

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -4,8 +4,9 @@ import "./Home.scss";
 // ASSETS
 
 // DEPENDANCIES
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
+import qs from "qs";
 
 // COMPONENTS
 import Card from "../../Components/Card/Card";
@@ -17,44 +18,82 @@ const Home = () => {
   const [characters, setCharacters] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [nameFilter, setNameFilter] = useState("");
+  const [numberOfResults, setNumberOfResults] = useState(0);
+  const [numberOfPages, setNumberOfPages] = useState(5);
+
+  const url = "http://localhost:5000/characters/?";
+  const location = useLocation(); // représente l'url
+  const params = qs.parse(location.search.substring(1)); // transforme "?page=1" en objet {page:1}
+  const page = parseInt(params.page) || 1;
+  const limit = parseInt(params.limit) || 5;
 
   useEffect(() => {
     const fetchData = async () => {
-      const url = "http://localhost:5000/";
-      const response = await axios.get(
-        url + "characters/?" + "name=" + nameFilter
-      );
+      const queryParams = qs.stringify({
+        page: page,
+        limit: limit,
+        name: nameFilter,
+      });
+
+      const response = await axios.get(url + queryParams);
 
       setCharacters(response.data.data);
+
+      setNumberOfResults(response.data.count);
+
+      setNumberOfPages(Math.ceil(response.data.count / limit));
 
       setIsLoading(false);
     };
 
     fetchData();
-  }, [nameFilter]);
+  }, [nameFilter, page, limit]);
+
+  const paginationLinks = [];
+
+  for (let i = 1; i <= numberOfPages; i++) {
+    paginationLinks.push(
+      <Link
+        className={
+          page === i || (!page && i === 1) ? "page-link active" : "page-link"
+        }
+        to={`?page=${i}`}
+      >
+        {i}
+      </Link>
+    );
+  }
 
   return (
     <div className="home">
-      <div className="filter">
-        <label>
-          Filtrer par nom :
-          <input
-            type="text"
-            value={nameFilter}
-            onChange={(e) => {
-              setNameFilter(e.target.value);
-            }}
-          ></input>
-        </label>
-      </div>
-      <div className="characters-cards">
-        {isLoading
-          ? "Chargement en cours..."
-          : characters.map((e, i) => (
-              <Link to={`/character/${e._id}`}>
-                <Card key={i} image={e.picture_url} title={e.name} />
-              </Link>
-            ))}
+      <div className="container">
+        <div className="filter">
+          <label>
+            Filtrer par nom :
+            <input
+              type="search"
+              value={nameFilter}
+              onChange={(e) => {
+                setNameFilter(e.target.value);
+              }}
+            ></input>
+          </label>
+        </div>
+
+        <div className="pagination">
+          <div>Nombre de résultats : {numberOfResults}</div>
+          <div className="page-links">{paginationLinks}</div>
+        </div>
+
+        <div className="characters-cards">
+          {isLoading
+            ? "Chargement en cours..."
+            : characters.map((e, i) => (
+                <Link className="character-card" to={`/character/${e._id}`}>
+                  <Card key={i} image={e.picture_url} title={e.name} />
+                </Link>
+              ))}
+        </div>
       </div>
     </div>
   );
